a1f8a4fc74a76ab1a6d034138c3d0a4df432d226
Dmitry Yalysina <61391319+LeoKikosh@users.noreply.github.com>
2022-08-19 22:27:33 +0300

    Create lost-without-a-map.js

---
+++ lost-without-a-map.js
@@ -0,0 +1,11 @@
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+function maps(numbers){
+  
+  const doubledNumbers = numbers.map(num => num * 2)
+  
+  return doubledNumbers
+
+}
