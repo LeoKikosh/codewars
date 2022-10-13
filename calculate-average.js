c782f214e9151c6168bfda46cd01b67b9120a3ec
Dmitry Yalysina <61391319+LeoKikosh@users.noreply.github.com>
2022-10-13 22:50:48 +0300

    Create calculate-average.js

---
+++ calculate-average.js
@@ -0,0 +1,12 @@
+//Write a function which calculates the average of the numbers in a given list.
+
+function find_average(array) {
+  if (array.length === 0) {
+  return 0;
+  }
+  var result = 0;
+  for (i=0; i<array.length; i++) {
+    result +=array[i];
+  }
+  return result/array.length;
+}
