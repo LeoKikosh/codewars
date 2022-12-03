57bd99a5f61b495551a6abd557c914d06d2b69f7
Dmitry Yalysina <61391319+LeoKikosh@users.noreply.github.com>
2022-12-03 20:29:28 +0300

    Create cockroach.js

---
+++ cockroach.js
@@ -0,0 +1,10 @@
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the // integer (= floored).
+// Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+function cockroachSpeed(s) {
+  const secsInHour = 3600;
+  const centimetersInKilometers = 100000;
+  
+  return Math.floor((s*centimetersInKilometers)/secsInHour);
+}
+
