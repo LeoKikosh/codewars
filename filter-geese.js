99d4029e777abfa2716b6eada9ce60078e3aafc4
Dmitry Yalysina <61391319+LeoKikosh@users.noreply.github.com>
2022-12-13 21:44:18 +0300

    Update filter-geese.js

--- filter-geese.js
+++ filter-geese.js
@@ -1,4 +1,4 @@
-//Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
 
 function gooseFilter (birds) {
   var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
