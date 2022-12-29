44809785978600e88bf89288739abb97dd3d9f38
Dmitry Yalysina <61391319+LeoKikosh@users.noreply.github.com>
2022-12-29 23:34:33 +0300

    Create switch-statement.js

---
+++ switch-statement.js
@@ -0,0 +1,18 @@
+// Complete the function howManydays. It accepts 1 parameter month, which means the month of the year. Different months have a different number of days as // shown in the table below. Return the number of days that are in month. There is no need for input validation: month will always be greater than 0 and //less than or equal to 12.
+
+function howManydays(month){
+
+  switch (month){
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30 
+      break
+    case 2: 
+      return 28
+      break
+    default:
+      return 31
+  }
+}
